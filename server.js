b94e0b829bb8b840f69120aad800a756d5c6d965
bartlomiejstachyra98 <bartlomiej.stachyra98@gmail.com>
2022-06-17 03:08:55 +0200

    commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+let date_ob = new Date();
+var satelize = require('satelize');
+var timezone;
+
+const date = new Date();
+
+
+app.get('/', function(req, res) {
+    const remoteAddress = req.socket.remoteAddress;
+    const array = remoteAddress.split(':')
+    const remoteIP = array[array.length - 1]
+    satelize.satelize({ip:remoteIP}, function(err, payload) {
+        timezone = payload.timezone;
+    });
+    res.send( ' Ip: ' + remoteIP + " data: " + date.toLocaleString('en-US', {
+        timeZone: timezone,
+        dateStyle: 'full',
+        timeStyle: 'full',
+    }),);
+});
+
+app.listen(3000, function() {
+    console.log(date_ob + ' autor: Bartlomiej Stachyra port: ', this.address().port);
+});
