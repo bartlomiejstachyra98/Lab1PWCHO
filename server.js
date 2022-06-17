cd5166c7e137ee686d61739ad6a2620e45283844
bartlomiejstachyra98 <bartlomiej.stachyra98@gmail.com>
2022-06-17 19:24:00 +0200

    da

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var app = express();
 let date_ob = new Date();
-var satelize = require('satelize');
-var timezone;
 
 const date = new Date();
+let timezone;
 
+const axios = require('axios');
+
+axios.get('http://ip-api.com/json')
+    .then(res => {
+        timezone = res.data.timezone;
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 app.get('/', function(req, res) {
     const remoteAddress = req.socket.remoteAddress;
     const array = remoteAddress.split(':')
     const remoteIP = array[array.length -1]
-    satelize.satelize({ip:remoteIP}, function(err, payload) {
-        timezone = payload.timezone;
-    });
+
+
     res.send( ' Ip: ' + remoteIP + " data: " + date.toLocaleString('en-US', {
         timeZone: timezone,
         dateStyle: 'full',
@@ -21,7 +28,7 @@ app.get('/', function(req, res) {
     }),);
 });
  
-const port = process.env.port || 5000;
+const port = process.env.port || 3500;
 
 app.listen(port, function() {
     console.log(date_ob + ' autor: Bartlomiej Stachyra port: ', this.address().port);
