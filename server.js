9b14087f25d343176227ea2dde270e8ceef13978
bartlomiejstachyra98 <bartlomiej.stachyra98@gmail.com>
2022-06-17 19:30:20 +0200

    da

--- server.js
+++ server.js
@@ -4,22 +4,19 @@ let date_ob = new Date();
 
 const date = new Date();
 let timezone;
-
+let remoteIP;
 const axios = require('axios');
 
 axios.get('http://ip-api.com/json')
     .then(res => {
         timezone = res.data.timezone;
+        remoteIP = res.data.query;
     })
     .catch(error => {
         console.error(error);
     });
 
 app.get('/', function(req, res) {
-    const remoteAddress = req.socket.remoteAddress;
-    const array = remoteAddress.split(':')
-    const remoteIP = array[array.length -1]
-
 
     res.send( ' Ip: ' + remoteIP + " data: " + date.toLocaleString('en-US', {
         timeZone: timezone,
